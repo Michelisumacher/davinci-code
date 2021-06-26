cb156803fc5e4605941b74d53813e9666b43aa02
Michelisumacher <86482795+Michelisumacher@users.noreply.github.com>
2021-06-26 22:26:34 +0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -60,15 +60,11 @@ Ip Local  : {socket.gethostbyname(socket.gethostname())}
 
 </p> 
 
-#### About Me :
 
-```bash
 
 <h3 align="left">🧑🏻‍💻 Languages and Tools:</h3>
 <p align="left"> <a href="https://www.w3schools.com/cpp/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://pytorch.org/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg" alt="pytorch" width="40" height="40"/> </a></p>
 
-
-```
 
 ''')
 f.close()
