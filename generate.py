083d149b0718f98677382589a897874a487812d1
Michelisumacher <86482795+Michelisumacher@users.noreply.github.com>
2021-06-26 22:22:03 +0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -13,10 +13,9 @@ f.write(f'''
 
 <p align="center">
 
-  <a href="https://github.com/{nameid}">
-    <img alt="GitHub Stats" src="https://github-readme-stats.vercel.app/api?username={nameid}&hide=issues&hide_title=true&include_all_commits=true&bg_color=30,e96443,904e95&title_color=fff&text_color=fff" />
-   </a>
-   
+  
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=scallions&label=Profile%20views&color=0e75b6&style=flat" alt="scallions" /> </p>
+
 #### This Page Create at:
 
 ```bash
@@ -49,7 +48,9 @@ Ip Local  : {socket.gethostbyname(socket.gethostname())}
 
 ```bash
 
-{nameid}
+<h3 align="left">🧑🏻‍💻 Languages and Tools:</h3>
+<p align="left"> <a href="https://www.w3schools.com/cpp/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://pytorch.org/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg" alt="pytorch" width="40" height="40"/> </a></p>
+
 
 ```
 
